8795992788c70d111178ffe297c2e627b8da976e
RyanGamingYT <rg373429@gmail.com>
2023-11-04 15:03:58 +0500

    commit # 5

--- MAC/MAC/settings.py
+++ MAC/MAC/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Managing Media
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = '/media/'
